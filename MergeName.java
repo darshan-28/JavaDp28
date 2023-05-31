c30cefe891db3a1a99a81bec61e3b582229d39b4
DARSHAN GAJANAN PADMANE <80152871+darshan-28@users.noreply.github.com>
2023-05-31 12:08:19 +0530

    Add files via upload

---
+++ MergeName.java
@@ -0,0 +1,14 @@
+class MergeName1
+{
+	public static void main(String[] args) 
+	{
+		mergeName();
+	}
+	public static void mergeName()
+		{
+			String a = "DArshan";
+			String b = "Padmane";
+			String ans = a+b;
+			System.out.println(ans);
+		}
+}
