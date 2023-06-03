a015434d784a1577324bc6988a5db2ee03a8f59d
DARSHAN GAJANAN PADMANE <80152871+darshan-28@users.noreply.github.com>
2023-06-04 00:48:56 +0530

    Add files via upload

---
+++ AddTwoNum1.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+class AddTwoNum1
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner (System.in);
+		
+		System.out.println("enter your first number:");
+        int ip1= sc.nextInt();
+        
+		System.out.println("enter your second number:");
+		int ip2= sc.nextInt();
+
+
+		System.out.println("the Sum of "+ip1+" and "+ip2+" is : "+(ip1+ip2)); 
+		
+
+//System.out.println("enter your first name:");
+//		String first = sc.next();
+		
+//		System.out.println("enter your middle name:");
+//		String middle = sc.next();
+//		
+//		System.out.println("enter your last name:");
+//		String last = sc.next();
+//		 
+		
+//		String FullName = first+" "+middle+" "+last;
+//		System.out.println("Your FullName is :"+FullName); -->
+
+
+	}
+}
