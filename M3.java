c5a55d80063de660967db44b6dec965a09116b17
Darshan Padmane <80152871+darshan-28@users.noreply.github.com>
2023-07-21 17:16:13 +0530

    Add files via upload

---
+++ M3.java
@@ -0,0 +1,24 @@
+class M3
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("from main()");
+		raju();
+		raju();
+		raju();
+		raju();
+		raju();
+
+		chutki();
+	}
+
+	public static void raju()
+	{
+		System.out.println("hii!");
+	}
+
+	public static void chutki()
+	{
+		System.out.println("Get Lost!");
+	}
+}
---
+++ M4.java
@@ -0,0 +1,20 @@
+class M4
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("from main()");
+
+		int a =10;
+		System.out.println(a);
+		demo();
+		 
+	}
+
+	public static void demo()
+	{
+		String a = "Qspiders";
+		System.out.println(a);
+	}
+
+	 
+}
