2f41a6bb2cc54a51b773d4acda3ceec118feeaff
Darshan Padmane <80152871+darshan-28@users.noreply.github.com>
2023-08-23 23:14:22 +0530

    Add files via upload

---
+++ xyz.java
@@ -0,0 +1,21 @@
+class x 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("from x");
+	}
+}
+class y
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("from y");
+	}
+}
+class z
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("from z");
+	}
+}
