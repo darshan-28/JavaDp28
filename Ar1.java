b92024a0326af47395a4de995ab8898d33f0ed15
Darshan Padmane <80152871+darshan-28@users.noreply.github.com>
2023-08-25 23:48:34 +0530

    Add files via upload

---
+++ Ar1.java
@@ -0,0 +1,15 @@
+class Ar1
+{
+	public static void main(String[] args) 
+	{
+		int []a=new int [10];
+		System.out.println("int :"+a);
+		double []d=new double [10];
+		System.out.println("double :"+d);
+		String []s=new String [10];
+		System.out.println("String :"+s);
+		float []f=new float [10];
+		System.out.println("float :"+ f);
+		
+	}
+}
