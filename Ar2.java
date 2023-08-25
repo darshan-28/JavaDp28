da4aae5beeaf80e5dbafa6089dfddd8af05acbae
Darshan Padmane <80152871+darshan-28@users.noreply.github.com>
2023-08-25 23:54:59 +0530

    Add files via upload

---
+++ Ar2.java
@@ -0,0 +1,25 @@
+class Ar2
+{
+	public static void main(String[] args) 
+	{
+		int []a=new int [5];
+		a[0]=100;
+		a[1]=1000;
+		a[2]=10000;
+		a[4]=100000;
+		System.out.println(a[0]);
+		System.out.println(a[1]);
+		System.out.println(a[2]);
+		System.out.println(a[3]);
+		System.out.println(a[4]);
+		//System.out.println(a[5]);
+
+		//double []d=new double [10];
+	//	System.out.println("double :"+d);
+		//String []s=new String [10];
+		//System.out.println("String :"+s);
+		//float []f=new float [10];
+		//System.out.println("float :"+ f);
+		
+	}
+}
